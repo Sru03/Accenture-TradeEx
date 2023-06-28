459d87a85e7f8cecfabf11c8fb304c1698d90701
Sru03 <94092731+Sru03@users.noreply.github.com>
2023-06-28 23:52:10 +1200

    marketPlace base set up

--- accenture-trade-ez/src/components/JobList.js
+++ accenture-trade-ez/src/components/JobList.js
@@ -8,10 +8,10 @@ function JobList({ jobs }) {
         return <div key={jobIndex} 
          style = {{width: '10%'}}>
             <div className = 'job-item'>
-            <img src={jobItem.url} width = "100%"/>
-            <p> {jobItem.title}</p>
-            <p> {jobItem.location}</p>
-            <p> ${jobItem.price}</p>
+            <img src={jobItem.image} width = "70%"/>
+            <p style = {{ height : '10px'}}> Job title:  {jobItem.title}</p>
+            <p style = {{border : " 1px solid black", borderRadius: " 20px"}}>{jobItem.location}</p>
+            <p style = {{border : " 1px solid black", borderRadius: " 10px"}}> ${jobItem.price}</p>
             </div>
 
             </div>
