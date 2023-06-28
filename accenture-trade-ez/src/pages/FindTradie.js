62152f9f8c93c1943e24c40f4ad0cef6bd314fc9
Elisa Bertoni <bertoni_elisa@hotmail.com>
2023-06-28 16:18:13 +1200

    add search tradie bar

--- accenture-trade-ez/src/pages/FindTradie.js
+++ accenture-trade-ez/src/pages/FindTradie.js
@@ -3,9 +3,14 @@ import React from 'react';
 
 const FindTradie = () => {
   return (
-    <div>
-
+    <>
+    <div className="search-tradie">
+      <input type="search" placeholder="What type of tradie are you looking for?" />
+      <button>Search</button>
     </div>
+
+    <button className='post-job'>POST A JOB</button>
+    </>
   );
 };
 
