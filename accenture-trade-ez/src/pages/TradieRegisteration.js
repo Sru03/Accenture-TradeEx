909a39d320a5ba455d3aec063fc2bf4ebf93f735
Sebastian Thomas <stho382@aucklanduni.ac.nz>
2023-06-28 13:32:41 +1200

    added textbox comp and tradie page

---
+++ accenture-trade-ez/src/pages/TradieRegisteration.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import TextBox from './TextBox';
+import FileInput from './FileInput';
+
+function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [pdfFile, setPdfFile] = useState(null);
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleFileChange = (e) => {
+    setPdfFile(e.target.files[0]);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append('name', name);
+    formData.append('email', email);
+    formData.append('pdf', pdfFile);
+
+    fetch('/register', {
+      method: 'POST',
+      body: formData,
+    })
+      .then((response) => response.text())
+      .then((message) => {
+        console.log(message);
+        // Show success message to the user
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        // Show error message to the user
+      });
+  };
+
+  return (
+    <div>
+      <h1>Registration Form</h1>
+      <form onSubmit={handleSubmit}>
+        <TextBox label="name" value={name} onChange={handleNameChange} />
+        <TextBox label="email" value={email} onChange={handleEmailChange} />
+        <FileInput label="pdf" onChange={handleFileChange} />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
