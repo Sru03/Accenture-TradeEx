3843efe9d9315b85524fc92f0545537b257f5ab7
Elisa Bertoni <bertoni_elisa@hotmail.com>
2023-06-28 15:00:53 +1200

    set up react router and move navbar to a different component

--- accenture-trade-ez/src/pages/HomePage.js
+++ accenture-trade-ez/src/pages/HomePage.js
@@ -1,27 +1,10 @@
 import React from 'react';
-import logo1 from '../assets/trade_ez_logonav.png';
 import logo2 from '../assets/main_logo.png';
 import SearchBar from '../components/SearchBar';
 
 const HomePage = () => {
   return (
     <div>
-      {/* Logo and Navbar */}
-      <div className="header-container">
-        {/* Logo */}
-        <div className="logo-container">
-          <img src={logo1} alt="TradeEz Logo" className="logo" />
-        </div>
-
-        {/* Navbar */}
-        <nav className="navbar">
-          <ul className="navbar-list">
-            <li className="navbar-item"><a href="#" className="navbar-link">Marketplace</a></li>
-            <li className="navbar-item"><a href="#" className="navbar-link">Find me a Tradie</a></li>
-            <li className="navbar-item"><a href="#" className="navbar-link">Register</a></li>
-          </ul>
-        </nav>
-      </div>
 
       {/* Business Logo */}
       <div className="business-logo-container">
