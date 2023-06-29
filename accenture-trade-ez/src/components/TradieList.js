b0179dac58e22b5249eba73563a7e6a154169caf
Elisa Bertoni <bertoni_elisa@hotmail.com>
2023-06-29 17:25:38 +1200

    style tradie container

--- accenture-trade-ez/src/components/TradieList.js
+++ accenture-trade-ez/src/components/TradieList.js
@@ -11,10 +11,10 @@ function TradieList({ tradie }) {
               <img src={tradieItem.profile_pic} alt="profile-pic" width = "100%" style = {{ borderRadius: "50px"}}/>
               <p style = {{ height : '10px', fontWeight: 'bold', fontSize: '30px'}}> {tradieItem.name}</p>
               <p style = {{border : " 1px solid black", borderRadius: " 20px", padding: '10px'}}>{tradieItem.job_title}</p>
-              <p >{tradieItem.location}</p>
-              <p style = {{border : " 1px solid black", borderRadius: "10px", fontSize: "18px",}}> {tradieItem.email}</p>
-              <p style = {{border : " 1px solid black", borderRadius: " 10px"}}> {tradieItem.phone_number}</p>
-              <p>${tradieItem.hourly_rate } p/h</p>
+              <p ><i class="fa-solid fa-location-dot" style = {{padding: '10px'}}></i>{tradieItem.location}</p>
+              <i class="fa-solid fa-phone" style = {{paddingRight: '40px'}}></i>
+              <i class="fa-solid fa-envelope"></i>
+              <p style = {{ color: '#FCA311'}}>${tradieItem.hourly_rate } p/h</p>
             </div>
 
           </div>
